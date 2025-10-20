64b6db6323e551eeff08cb02cc7b383e75881983
Nestor Andres Martinez <na.martinezv1@uniandes.edu.co>
2025-10-20 01:14:04 +0000

    actualizar version

--- endpoints.py
+++ endpoints.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 class Endpoint(Resource):
     def get(self):
-        return {"message": "Blacklist Uniandes V2 - Grupo Nestor Andres Martinez, Juan Alberto Tapia, Sergio Andres Gelvez y Alberto Silva Rueda"},200
+        return {"message": "Blacklist Uniandes V3 (Rolling with Additional Batch) - Grupo Nestor Andres Martinez, Juan Alberto Tapia, Sergio Andres Gelvez y Alberto Silva Rueda"},200
 
 
 class Blacklists(Resource):
