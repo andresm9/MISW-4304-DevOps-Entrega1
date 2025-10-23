66c8bebbc7c1c88fa7c163493349a467e6a5a7bb
Nestor Andres Martinez <na.martinezv1@uniandes.edu.co>
2025-10-23 18:47:28 +0000

    agregar un nuevo testcase

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -5,6 +5,7 @@ def test_root_endpoint(client):
     assert response.status_code == 200
     assert 'message' in response.get_json()
 
+
 def test_not_exists_email(client, token):
     """comprobar que un email que no ha sido agregado no existe en la blacklist"""
 
@@ -14,6 +15,7 @@ def test_not_exists_email(client, token):
     assert response.status_code == 200
     assert response.get_json()['exist'] == False
 
+
 def test_exists_email(client, token):
     """comprobar que un email que ha sido agregado existe en la blacklist"""
 
@@ -22,6 +24,7 @@ def test_exists_email(client, token):
 
     assert response.status_code == 200
     assert response.get_json()['exist'] == True
+
 
 def test_add_email_to_blacklist(client, token):
     """comprobar que un email fue agregado correctamente a la blacklist"""
@@ -36,6 +39,7 @@ def test_add_email_to_blacklist(client, token):
     # assert response.status_code == 201
     assert response.get_json()['message'] == "Email Blacklisted Successfully"
 
+
 def test_add_duplicate_email_to_blacklist(client, token):
     """comprobar que un email duplicado no puede ser agregado a la blacklist"""
 
@@ -48,6 +52,7 @@ def test_add_duplicate_email_to_blacklist(client, token):
     response = client.post("/blacklists", json=data, headers=headers)
     assert response.status_code == 400
     assert response.get_json()['message'] == "Email Already Blacklisted"
+
 
 def test_add_invalid_email_to_blacklist(client, token):
     """comprobar que un email inválido no puede ser agregado a la blacklist"""
@@ -62,6 +67,21 @@ def test_add_invalid_email_to_blacklist(client, token):
     assert response.status_code == 400
     assert "Not a valid email address" in response.get_json()['message']
 
+
+def test_add_empty_email_to_blacklist(client, token):
+    """comprobar que un email vacío no puede ser agregado a la blacklist"""
+
+    headers = {"Authorization": f"Bearer {token}"}
+    data = {
+        "email": "",
+        "app_uuid": "d0e92170-b0f9-48bb-8b4a-26c5e241d736",
+        "blocked_reason": "An Example Reason"
+    }
+    response = client.post("/blacklists", json=data, headers=headers)
+    assert response.status_code == 400
+    assert "Not a valid email address" in response.get_json()['message']
+
+
 def test_add_invalid_uuid_to_blacklist(client, token):
     """comprobar que un UUID inválido no puede ser agregado a la blacklist"""
 
@@ -74,6 +94,7 @@ def test_add_invalid_uuid_to_blacklist(client, token):
     response = client.post("/blacklists", json=data, headers=headers)
     assert response.status_code == 400
     assert "Not a valid UUID" in response.get_json()['message']
+
 
 def test_add_invalid_blocked_reason_to_blacklist(client, token):
     """comprobar que una razón de bloqueo inválida no puede ser agregada a la blacklist"""
